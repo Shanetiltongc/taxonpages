e00654f2c13b0d3548e1e5cbc99d6f63b6befa0d
Shanetiltongc <shanetiltongc@gmail.com>
2024-12-19 13:29:37 -0800

    Create LayoutFooter.vue

---
+++ src/layout/LayoutFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="p-4 text-center" :style="footerStyle">
+    <p>© 2024 UNH Entomology Lab. All rights reserved.</p>
+  </footer>
+</template>
+
+<script setup>
+import { computed, defineProps } from 'vue'
+
+// Accept Theme as a Prop
+defineProps({
+  theme: {
+    type: String,
+    required: true
+  }
+})
+
+// Dynamically Adjust Footer Style
+const footerStyle = computed(() => ({
+  backgroundColor: `var(--color-base-background)`,
+  color: `var(--color-base-content)`,
+  borderTop: `1px solid var(--color-base-border)`
+}))
+</script>
